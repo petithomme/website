883c4340e1ffbb6dde5adc9774e89afa4c6605aa
petithomme <nbpetithomme@gmail.com>
2024-02-15 11:32:05 +0100

    Cleaning

---
+++ src/components/subComponents/CertificationComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <img :src="require(`@/assets/certifications/${image}`)" />
+    <a :href="url">{{ name }}</a>
+</template>
+
+<script>
+    export default {
+        name: "CertificationComponent",
+        props: ["image", "name", "url"]
+    }
+</script>
+
+<style scoped>
+    img{
+        width: 3rem;
+        margin-right: 1rem;
+    }
+
+</style>
--- src/components/subComponents/DegreesComponent.vue
+++ src/components/subComponents/DegreesComponent.vue
@@ -3,51 +3,46 @@
 
         <h2> Blockchain </h2>
         <div v-for="(item, index) in blockchain" v-bind:key="index" class="certif">
-            <img :src="require(`@/assets/certifications/${item.image}`)" />
-            <a :href="item.url">{{ item.name }}</a>
+            <CertificationComponent :image="item.image" :name="item.name" :url="item.url" />
         </div>
 
         <h2> Professionals </h2>
         <div v-for="(item, index) in professionals" v-bind:key="index" class="certif">
-            <img :src="require(`@/assets/certifications/${item.image}`)" />
-            <a :href="item.url">{{ item.name }}</a>
+            <CertificationComponent :image="item.image" :name="item.name" :url="item.url" />
         </div>
 
         <h2> Udemy </h2>
         <div v-for="(item, index) in udemy" v-bind:key="index" class="certif">
-            <img :src="require(`@/assets/certifications/${item.image}`)" />
-            <a :href="item.url">{{ item.name }}</a>
+            <CertificationComponent :image="item.image" :name="item.name" :url="item.url" />
         </div>
 
         <h2> Udacity </h2>
         <div v-for="(item, index) in udacity" v-bind:key="index" class="certif">
-            <img :src="require(`@/assets/certifications/${item.image}`)" />
-            <a :href="item.url">{{ item.name }}</a>
+            <CertificationComponent :image="item.image" :name="item.name" :url="item.url" />
         </div>
 
         <h2> Eduonix </h2>
         <div v-for="(item, index) in eduonix" v-bind:key="index" class="certif">
-            <img :src="require(`@/assets/certifications/${item.image}`)" />
-            <a :href="item.url">{{ item.name }}</a>
+            <CertificationComponent :image="item.image" :name="item.name" :url="item.url" />
         </div>
 
         <h2> Coursera </h2>
         <div v-for="(item, index) in coursera" v-bind:key="index" class="certif">
-            <img :src="require(`@/assets/certifications/${item.image}`)" />
-            <a :href="item.url">{{ item.name }}</a>
+            <CertificationComponent :image="item.image" :name="item.name" :url="item.url" />
         </div>
 
         <h2> Edx </h2>
         <div v-for="(item, index) in edx" v-bind:key="index" class="certif">
-            <img :src="require(`@/assets/certifications/${item.image}`)" />
-            <a :href="item.url">{{ item.name }}</a>
+            <CertificationComponent :image="item.image" :name="item.name" :url="item.url" />
         </div>
     </div>
 </template>
 
 <script>
+    import CertificationComponent from "@/components/subComponents/CertificationComponent";
     export default {
         name: "DegreesComponent",
+        components: {CertificationComponent},
         props: ["udemy", "udacity", "professionals", "eduonix", "coursera", "edx", "blockchain"],
         data() {
             return {
@@ -74,8 +69,4 @@
         align-items: center;
     }
 
-    img{
-        width: 3rem;
-        margin-right: 1rem;
-    }
 </style>
